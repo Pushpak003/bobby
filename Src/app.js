aa8ee7c66d398bffd7b1c9728a83b5f969926450
Pushpak003 <pushpakpathe63@gmail.com>
2025-06-15 17:32:16 +0530

    added limits

--- Src/app.js
+++ Src/app.js
@@ -3,13 +3,13 @@ const cors = require("cors");
 const app = express();
 const path = require("path");
 // Add this middleware before routes
-app.use("/api/payment-webhook", express.raw({ type: "application/json" }));
+app.use("/api/payment-webhook", express.raw({ type: "application/json",limit: "5mb"  }));
 
 // Your existing middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: "10mb"} ));
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true,limit: "10mb" }));
 app.use("/invoices", express.static(path.join(__dirname, "public/invoices")));
 // Routes
 app.use("/api", require("./routes/payment.routes"));
